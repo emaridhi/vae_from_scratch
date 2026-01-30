bfc96f318592d3dc806e026ea69ae2add9a5f93e
emaridhi <emaridhi@gmail.com>
2026-01-29 22:00:39 -0500

    completed model.py

---
+++ model.py
@@ -0,0 +1,48 @@
+#builsing a probabilistic network
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Encoder(nn.Module):
+    def __init__(self, input_dim=784, hidden_dim=400, latent_dim=20):
+        super().__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc_mu = nn.Linear(hidden_dim, latent_dim)
+        self.fc_logvar = nn.Linear(hidden_dim, latent_dim)
+
+    def forward(self, x):
+        h = F.relu(self.fc1(x)) #activation function
+        mu = self.fc_mu(h)
+        logvar = self.fc_logvar(h)
+        return mu, logvar #output is not what z is rather mentions its a gaussian distr with mean mu and variance sigma sq
+
+
+class Decoder(nn.Module):
+    def __init__(self, latent_dim=20, hidden_dim=400, output_dim=784):
+        super().__init__()
+        self.fc1 = nn.Linear(latent_dim, hidden_dim)
+        self.fc_out = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, z):
+        h = F.relu(self.fc1(z))
+        x_hat = torch.sigmoid(self.fc_out(h)) #forces output in [0, 1]
+        return x_hat #reconstruts the input
+    
+
+def reparameterize(mu, logvar):
+    std = torch.exp(0.5 * logvar)
+    eps = torch.randn_like(std) #creating noise. sam eshape as std normal
+    return mu + eps * std #sampling is written as a deterministic fn + noise
+
+
+class VAE(nn.Module): # x -> encoder  -> reparametize -> x hat
+    def __init__(self):
+        super().__init__()
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+
+    def forward(self, x):
+        mu, logvar = self.encoder(x)
+        z = reparameterize(mu, logvar)
+        x_hat = self.decoder(z)
+        return x_hat, mu, logvar
